4485276020cb6549bd5987664fcfb159d07bc10f
vinaysakariyatest <sakariyavinay1@gmail.com>
2024-06-12 11:47:39 +0530

    changes

--- routes/admin.js
+++ routes/admin.js
@@ -11,11 +11,11 @@ router.post('/login',admin.login)
 router.post('/category',auth,admin.addCategory)
 router.put('/category/:id',auth,admin.editCategory)
 router.delete('/category/:id',auth,admin.deleteCategory)
-router.get('/category',auth,admin.viewCategory)
+router.get('/category',admin.viewCategory)
 
 // Books
 router.post('/books',auth,admin.addBook)
-router.get('/books',auth,admin.viewAllBooks)
+router.get('/books',admin.viewAllBooks)
 router.delete('/books/:id',auth,admin.deleteBook)
 router.put('/books/:id',auth,admin.editBook)
 
